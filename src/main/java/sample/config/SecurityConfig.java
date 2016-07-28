af6f57176f6d24ec33efa96b2809b1fbf1d39a5a
Joe Grandja <jgrandja@pivotal.io>
2016-07-28 10:39:13 -0400

    Section 2.1 CSRF with AngularJS

--- src/main/java/sample/config/SecurityConfig.java
+++ src/main/java/sample/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author Rob Winch
@@ -37,7 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 					.antMatchers("/assets/**", "/webjars/**").permitAll()
 					.anyRequest().authenticated().and()
-				.httpBasic();
+				.httpBasic().and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
 	@Bean
