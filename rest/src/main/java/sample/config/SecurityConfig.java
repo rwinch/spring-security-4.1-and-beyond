a8d61614a5ff3b8460db317c72835e82b8931bde
Rob Winch <rwinch@gopivotal.com>
2016-08-02 22:10:40 -0700

    Section 6.3 BCrypt

--- rest/src/main/java/sample/config/SecurityConfig.java
+++ rest/src/main/java/sample/config/SecurityConfig.java
@@ -15,10 +15,12 @@
  */
 package sample.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
@@ -53,4 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						"style-src 'self' 'unsafe-inline'");
 	}
 	// @formatter:on
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
